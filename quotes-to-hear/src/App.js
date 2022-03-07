7d69fa128da00a1dcd70004113b2de2482c357b9
KevinyTeixeira <keviny.t3@gmail.com>
2022-03-07 17:21:45 -0300

    Github Pages test

--- quotes-to-hear/src/App.js
+++ quotes-to-hear/src/App.js
@@ -4,7 +4,7 @@ import { GlobalStyle } from './globalStyle';
 import { dsv } from 'd3';
 import styled from 'styled-components';
 
-const csvUrl  =  ('https://gist.githubusercontent.com/KevinyTeixeira/bca251b9b051c20260dc067b0508cc25/raw/62da06c488dc20c39d736980d99757435073cb34/Speaks.csv');
+const csvUrl  =  process.env.REACT_APP_CSVDB;
 
 export const App = () => {
 
